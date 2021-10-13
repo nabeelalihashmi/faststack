3274977a09a39f9cb5fc79929b7c273d3144ff5c
Nabeel Ali <mail2nabeelali@gmail.com>
2021-10-13 09:29:17 +0500

    getting away from fastify cli

--- app.js
+++ app.js
@@ -3,11 +3,58 @@
 const path = require('path')
 const AutoLoad = require('fastify-autoload')
 
+
+// const fastify = require('fastify')({logger: true});
+
+// fastify.register(require('fastify-static'), {
+//   root: path.join(__dirname, 'public'),
+//   // prefix: '/public/', // optional: default '/'
+// })
+
+// fastify.register(AutoLoad, {
+//   dir: path.join(__dirname, 'plugins'),
+// })
+
+// fastify.register(AutoLoad, {
+//   dir: path.join(__dirname, '/app/routes'),
+// })
+
+
+
+// // This is to use pen di siri mongoose
+
+// async function StartMongo() {
+//   const uri = process.env.MONGO_URI || null;
+//   if (uri != null) {
+//     const mongoose = require('mongoose');
+//     try {
+//       await mongoose.connect(uri)
+//       console.log('Mongodb Connected')
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
+
+// async function StartFastify() {
+//   try {
+//     const port = process.env.PORT || 3000
+//     await fastify.listen(port, () => {console.log('Server Started')})
+//   } catch (error) {
+//     console.log('error: ', error)
+//     process.exit(1)
+//   }
+// }
+
+// StartFastify();
+// StartMongo();
+
+
 module.exports = async function (fastify, opts) {
 
   fastify.logger = true;
 
-  
+
   // Place here your custom code!
   // This load fastify poly toly svelte
   fastify.register(require('fastify-static'), {
@@ -44,7 +91,6 @@ module.exports = async function (fastify, opts) {
     } catch (error) {
       console.log(error);
     }
-
   }
 
 }
